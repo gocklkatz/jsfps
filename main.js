8bf1eeb6b1e6172ae77737a40ea281729a356a39
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2024-11-29 18:26:40 +0100

    The green cube

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
-// Scene
-const scene = new THREE.Scene()
+if ( WebGL.isWebGL2Available() ) {
 
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
+    renderer.setAnimationLoop( animate );
+    document.getElementById( 'container' ).appendChild( renderer.domElement );
+    
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    
+    camera.position.z = 3;
+    
+    function animate() {
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+    
+        renderer.render( scene, camera );
+    }
+
+} else {
+
+	const warning = WebGL.getWebGL2ErrorMessage();
+	document.getElementById( 'container' ).appendChild( warning );
+
+}
